51faae9d572f8cca0a7bfb5cd274c64680f2cb57
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-21 16:33:44 +0100

    idex = 1000

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 
 //Scorro index da 1 a 100 e lo metto a video da 1 a 100
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 1000; i++) {
     //metto a video "Fizz" per index multipli 3
     //metto a video "Buzz" per index multipli 5
     //metto a video "FizzBuzz" per index multipli 3 e 5
