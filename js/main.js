69dd8a3cf319f78d4e959960769e8db49f58a681
Chrissx77 <garozzochristian@gmail.com>
2023-11-21 15:45:53 +0100

    added for 1 to 100

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Scorro in index da 1 a 100 e lo metto a video da 1 a 100
+for(let i = 1; i<= 100; i++){
+    //metto a video "Fizz" per i numeri multipli 3
+
+
+
+    //metto a video "Buzz" per i numeri multipli 5
+    //metto a video "FizzBuzz" per i numeri multipli 3 e 5
+
+
+
+
+    //metto a video tutti i restanti
+    console.log(i);
+}
