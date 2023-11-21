b7dcf9cbc323b03b2804c74e6e8bebcf2ea2053c
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-21 16:29:50 +0100

    added js logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
-//Scorro in index da 1 a 100 e lo metto a video da 1 a 100
-for(let i = 1; i<= 100; i++){
-    //metto a video "Fizz" per i numeri multipli 3
+const container = document.querySelector(".container");
 
-
-
-    //metto a video "Buzz" per i numeri multipli 5
-    //metto a video "FizzBuzz" per i numeri multipli 3 e 5
-
-
-
-
+//Scorro index da 1 a 100 e lo metto a video da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    //metto a video "Fizz" per index multipli 3
+    //metto a video "Buzz" per index multipli 5
+    //metto a video "FizzBuzz" per index multipli 3 e 5
     //metto a video tutti i restanti
-    console.log(i);
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.append("FizzBuzz");
+        square.style.backgroundColor = "green"
+        container.append(square);
+    } else if (i % 5 === 0) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.append("Fizz");
+        square.style.backgroundColor = "red";
+        container.append(square);
+    } else if (i % 3 === 0) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.append("Buzz");
+        square.style.backgroundColor = "orange";
+        container.append(square);
+    } else {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.append(i);
+        container.append(square);
+    }
+
 }
