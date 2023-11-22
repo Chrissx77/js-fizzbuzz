c279a06f6dedde4c3165f892b524dd1e1fcb4624
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-22 10:05:13 +0100

    fix ottimizzazione

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -7,29 +7,20 @@ for (let i = 1; i <= 1000; i++) {
     //metto a video "FizzBuzz" per index multipli 3 e 5
     //metto a video tutti i restanti
 
+    const square = document.createElement("div");
+    square.classList.add("square");
+
     if (i % 3 === 0 && i % 5 === 0) {
-        const square = document.createElement("div");
-        square.classList.add("square");
         square.append("FizzBuzz");
         square.style.backgroundColor = "green"
-        container.append(square);
     } else if (i % 5 === 0) {
-        const square = document.createElement("div");
-        square.classList.add("square");
         square.append("Buzz");
         square.style.backgroundColor = "red";
-        container.append(square);
     } else if (i % 3 === 0) {
-        const square = document.createElement("div");
-        square.classList.add("square");
         square.append("Fizz");
         square.style.backgroundColor = "orange";
-        container.append(square);
     } else {
-        const square = document.createElement("div");
-        square.classList.add("square");
         square.append(i);
-        container.append(square);
     }
-
+    container.append(square);
 }
